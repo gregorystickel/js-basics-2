18015bbeda1fdddc9c6f05f79d07252f82c319d0
Greg Stickel <gregory.stickel@live.com>
2022-09-23 10:47:59 -0400

    home.js done

--- assessment-js-basics-2/home.js
+++ assessment-js-basics-2/home.js
@@ -7,7 +7,6 @@
     page of the restaurant's website.
 */
 
-
 //////////////////PROBLEM 1////////////////////
 /* 
     Write an *arrow* function called `greetUser`
@@ -22,10 +21,10 @@
 */
 
 //CODE HERE
-
-
-
-
+let greetUser = (username) => {
+  console.log(`Welcome back, ${username}`);
+};
+greetUser(`Andrew`);
 
 //////////////////PROBLEM 2////////////////////
 /* 
@@ -47,11 +46,19 @@
         // `You're in our delivery zone!`
 */
 
-const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
+const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206];
 
 //CODE HERE
 
-
+function canWeDeliver(zipCode) {
+  for (i = 0; i < deliveryAreaZipCodes.length; i++) {
+    if (zipCode === deliveryAreaZipCodes[i])
+      return console.log(`You're in our delivery zone!`);
+  }
+  return console.log(`Sorry, we can't deliver to that address`);
+}
+canWeDeliver(84606);
+canWeDeliver(85205);
 
 /* 
     Problem 2 Continued
@@ -69,9 +76,16 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
     loop (for loop, higher order array method).
     Name your new function `canWeDeliverTwo`.
 */
+function canWeDeliverTwo(zipcode) {
+  if (deliveryAreaZipCodes.includes(zipcode)) {
+    return console.log(`You're in our delivery zone!`);
+  }
+  return console.log(`Sorry, we can't deliver to that address`);
+}
+canWeDeliverTwo(84606);
+canWeDeliverTwo(85205);
 
 // CODE HERE
-
 
 //////////////////PROBLEM 3////////////////////
 /* 
@@ -87,15 +101,15 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 */
 
 const deals = [
-    {
-        title: '15% Off!', 
-        desc: 'Applied to your entire order when you spend $30 or more'
-    }, 
-    {
-        title: 'Free Kids Meal with 2 Regular Entrees', 
-        desc: '   This deal lasts until the end of March! '
-    }
-]
+  {
+    title: "15% Off!",
+    desc: "Applied to your entire order when you spend $30 or more",
+  },
+  {
+    title: "Free Kids Meal with 2 Regular Entrees",
+    desc: "   This deal lasts until the end of March! ",
+  },
+];
 
 /*
     The owner has decided to take the 15% off
@@ -108,7 +122,7 @@ const deals = [
 
 //CODE HERE
 
-
+deals[0].title = deals[0].title.replace(`15%`, `10%`);
 
 /*
     The restaurant is going to continue its
@@ -124,3 +138,6 @@ const deals = [
 */
 
 //CODE HERE
+deals[1].desc = deals[1].desc.replace(`March`, `April`);
+
+console.log(deals);
